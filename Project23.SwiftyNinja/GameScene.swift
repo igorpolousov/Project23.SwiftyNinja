2b89bc3afa8752d1c836d8c74a10828d0cd0f3c4
Igor Polousov <igorpolousov@gmail.com>
2022-01-07 15:27:12 +0300

    Initial Commit

---
+++ Project23.SwiftyNinja/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Project23.SwiftyNinja
+//
+//  Created by Igor Polousov on 07.01.2022.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    
+    }
+
+}
